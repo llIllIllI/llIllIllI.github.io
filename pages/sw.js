a0e1c84de655ee70b0334bd9e676de933f1b73c3
Jiyeon <ji89yeon@gmail.com>
2026-03-31 17:00:52 +0900

    Update sw.js

--- pages/sw.js
+++ pages/sw.js
@@ -1,7 +1,8 @@
-// assets/js/sw.js
+// sw.js
 self.addEventListener('install', (e) => self.skipWaiting());
 self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
 
+// 페이지(index.html)에서 보낸 알림 명령 수신
 self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'SHOW_NOTIFICATION') {
         const { title, body } = event.data;
@@ -10,7 +11,8 @@ self.addEventListener('message', (event) => {
             icon: 'https://cdn-icons-png.flaticon.com/512/311/311074.png',
             vibrate: [200, 100, 200],
             badge: 'https://cdn-icons-png.flaticon.com/512/311/311074.png',
-            tag: 'smart-alarm'
+            tag: 'smart-alarm-sync',
+            renotify: true
         });
     }
 });
